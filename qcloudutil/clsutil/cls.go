8fa662c6c85953abbbb02b86679bebea8f4bf1d7
fhffly <hf_feng@163.com>
2023-05-11 14:55:31 +0800

    feat: clsutil add param - service

--- qcloudutil/clsutil/cls.go
+++ qcloudutil/clsutil/cls.go
@@ -17,13 +17,14 @@ var warnStr = "warn"
 var infoStr = "info"
 var debugStr = "debug"
 
+var serviceStr = "service"
 var urlStr = "url"
 var methodStr = "method"
-var stackStr = "stack"
 var bodyStr = "body"
+var stackStr = "stack"
 var userStr = "user"
-var levelStr = "level"
 var paramStr = "param"
+var levelStr = "level"
 var operationStr = "operation"
 var messageStr = "message"
 
@@ -33,11 +34,12 @@ var SendWarn = SendLog(warnStr)
 var SendInfo = SendLog(infoStr)
 var SendDebug = SendLog(debugStr)
 
-var SendLog = func(level string) func(client *cls.AsyncProducerClient, topic, url, method, body, param, op, user, msg, stack string) {
-	return func(client *cls.AsyncProducerClient, topic, url, method, param, body, op, user, msg, stack string) {
+var SendLog = func(level string) func(client *cls.AsyncProducerClient, topic, service, url, method, body, param, op, user, msg, stack string) {
+	return func(client *cls.AsyncProducerClient, topic, service, url, method, param, body, op, user, msg, stack string) {
 		fmt.Println("sending a log")
 		now := time.Now().Unix()
 		client.SendLog(topic, &cls.Log{Contents: []*cls.Log_Content{
+			{Key: &serviceStr, Value: &service},
 			{Key: &urlStr, Value: &url},
 			{Key: &methodStr, Value: &method},
 			{Key: &bodyStr, Value: &body},
@@ -51,9 +53,9 @@ var SendLog = func(level string) func(client *cls.AsyncProducerClient, topic, ur
 	}
 }
 
-func LoggerCtx(logger *cls.AsyncProducerClient, topicID, key string) func(http.Handler) http.Handler {
+func LoggerCtx(client *cls.AsyncProducerClient, topicID, key, service string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		if logger != nil && topicID != "" {
+		if client != nil && topicID != "" {
 			auth := authenticator.Authenticator{Key: []byte(key)}
 
 			fn := func(w http.ResponseWriter, r *http.Request) {
@@ -64,11 +66,11 @@ func LoggerCtx(logger *cls.AsyncProducerClient, topicID, key string) func(http.H
 							user := midacontext.ParseToken(r, auth)
 							reader := r.Body
 							if reader == nil {
-								SendFatal(logger, topicID, r.URL.String(), r.Method, "", "", "", user.String(), "", debugStack)
+								SendFatal(client, topicID, service, r.URL.String(), r.Method, "", "", "", user.String(), "", debugStack)
 							} else {
 								bodyRaw, _ := io.ReadAll(reader)
 								body := string(bodyRaw)
-								SendFatal(logger, topicID, r.URL.String(), r.Method, body, "", "", user.String(), "", debugStack)
+								SendFatal(client, topicID, service, r.URL.String(), r.Method, body, "", "", user.String(), "", debugStack)
 								reader.Close()
 							}
 						}
