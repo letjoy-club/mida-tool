829cca36cdce716fc3c1d7aea3aae0f96b102d88
zhenjiangjie <moevery@gmail.com>
2023-07-28 20:23:40 +0800

    feat: keyer

---
+++ keyer/keyer.go
@@ -0,0 +1,17 @@
+package keyer
+
+func User(userID string) string {
+	return "user:" + userID
+}
+
+func UserMatching(userID string) string {
+	return "user:" + userID + ":matching"
+}
+
+func UserMotion(userID string) string {
+	return "user:" + userID + ":motion"
+}
+
+func UserQuota(userID string) string {
+	return "user:" + userID + ":quota"
+}
